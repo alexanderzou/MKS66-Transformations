4cbc260d1847dc1d03a603e1195eb27afa35e034
alexanderzou <azou00stuy.edu>
2020-02-28 10:32:46 -0500

    (hopefully) transformations complete

--- matrix.py
+++ matrix.py
@@ -16,13 +16,28 @@ def make_scale( x, y, z ):
     return [[x,0,0,0],[0,y,0,0],[0,0,z,0],[0,0,0,1]]
 
 def make_rotX( theta ):
-    pass
+    sin = math.sin(math.radians(theta))
+    cos = math.cos(math.radians(theta))
+    return [[1,0,0,0],
+            [0,cos,-1*sin,0],
+            [0,sin,cos,0],
+            [0,0,0,1]]
 
 def make_rotY( theta ):
-    pass
+    sin = math.sin(math.radians(theta))
+    cos = math.cos(math.radians(theta))
+    return [[cos,0,sin,0],
+            [0,1,0,0],
+            [-1*sin,0,cos,0],
+            [0,0,0,1]]
 
 def make_rotZ( theta ):
-    return [[]]
+    sin = math.sin(math.radians(theta))
+    cos = math.cos(math.radians(theta))
+    return [[cos,-1*sin,0,0],
+            [sin, cos, 0, 0],
+            [0,0,1,0],
+            [0,0,0,1]]
 
 #print the matrix such that it looks like
 #the template in the top comment
